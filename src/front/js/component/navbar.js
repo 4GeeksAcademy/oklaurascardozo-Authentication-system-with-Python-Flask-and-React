4998b3ef6cb9e82819867cc7da9b6882743e10f9
Laura Cardozo <143517864+Laurascardozo@users.noreply.github.com>
2025-04-03 15:24:29 +0000

    Things fixed

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -16,7 +16,7 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">AUTENTICACIÓN</span>
 				</Link>
 				<div className="ml-auto">
 					{store.auth ? <button className="btn btn-primary" onClick={()=>handelLogout()}>Cerrar sesion</button>: null}
